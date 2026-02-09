3cd4a4399b2a7c64d1f547b3ee3a714833ebc729
Sumana Dey <sumanadey626@gmail.com>
2026-02-09 22:24:57 +0530

    Day 2

--- src/main.py
+++ src/main.py
@@ -21,9 +21,44 @@ def read_patient_data(file_path):
         logging.error(f"Unexpected error: {e}")
     return records
 
+def validate_patient_data(records):
+    valid_records = []
+    invalid_records = []
+
+    for record in records:
+        try:
+            patient_id = record.get('patient_id')
+            age = record.get('age')
+            diagnosis = record.get('diagnosis')
+
+            #validation rules
+            if not patient_id:
+                raise ValueError("Missing patient_id")
+            
+            if not age or not age.isdigit():
+                raise ValueError("Invalid age")
+            
+            if not diagnosis:
+                raise ValueError("Missing diagnosis")
+            
+            valid_records.append(record)
+
+        except Exception as e:
+            logging.warning(f"Invalid record {record} - Reason: {e}")
+            invalid_records.append(record)
+
+    return valid_records, invalid_records
+        
+
+
 def main():
     data = read_patient_data('../data/sample_data.csv')
-    logging.info(f"Total records received: {len(data)}")
+    
+    valid_data, invalid_data = validate_patient_data(data)
+
+    logging.info(f"Total records recieved: {len(data)}")
+    logging.info(f"Valid records count: {len(valid_data)}")
+    logging.info(f"Invalid records count: {len(invalid_data)}")
 
 if __name__ == "__main__":
     main()
